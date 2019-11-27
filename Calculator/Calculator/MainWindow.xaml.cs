1623ec24e14e4bc19d8857f296d262222173556a
PerunPlayer <prd.6666@abv.bg>
2019-11-27 15:24:07 +0200

    New functionalities

--- Calculator/Calculator/MainWindow.xaml.cs
+++ Calculator/Calculator/MainWindow.xaml.cs
@@ -16,11 +16,17 @@ namespace Calculator
             SUBSTRACT,
             MULTIPLY,
             DEVIDE,
+            EXP,
+            SIN,
+            COS,
+            SQRT,
+            LOG,
+            RECIPROCALLI,
             NO_OP
         }
 
         double input = 0, result = 0;
-        double number1 = 0, number2 = 0;
+        bool isReal = false;
         Operation operation = Operation.NO_OP;
         #endregion
         public MainWindow()
@@ -35,6 +41,15 @@ namespace Calculator
             {
                 txtInput.Text = digit;
             }
+            else if ((digit.Equals(".")) && (!isReal))
+            {
+                isReal = true;
+                txtInput.Text += digit;
+            }
+            else if ((digit.Equals(".")) && (isReal))
+            {
+                txtInput.Text = txtInput.Text;
+            }
             else
             {
                 txtInput.Text += digit;
@@ -43,7 +58,7 @@ namespace Calculator
 
         private void SaveOperation(string opCode)
         {
-            input = Double.Parse(txtInput.Text);
+            input = double.Parse(txtInput.Text, System.Globalization.CultureInfo.InvariantCulture);
             switch (opCode)
             {
                 case "+": operation = Operation.ADD; break;
@@ -55,7 +70,35 @@ namespace Calculator
                     break;
             }
             txtInput.Text = "0";
+            isReal = false;
         }
+
+        private void SaveOperationOP(string opCode)
+        {
+            switch (opCode)
+            {
+                case "EXP": operation = Operation.EXP; break;
+                case "SIN": operation = Operation.SIN; break;
+                case "COS": operation = Operation.COS; break;
+                case "SQRT": operation = Operation.SQRT; break;
+                case "LOG": operation = Operation.LOG; break;
+                case "1/x": operation = Operation.RECIPROCALLI; break;
+                default: operation = Operation.NO_OP; break;
+            }
+        }
+
+        private void Clean(string bttnCode)
+        {
+            input = Double.Parse(txtInput.Text);
+            switch (bttnCode)
+            {
+                case "CE": input = 0; txtInput.Text = "0"; break;
+                case "C": input = 0; result = 0; txtInput.Text = "0"; break;
+                default: break;
+            }
+            isReal = false;
+        }
+
         #endregion
 
         #region Operations
@@ -71,7 +114,7 @@ namespace Calculator
 
         private void Compute_Click(object sender, RoutedEventArgs e)
         {
-            result = Double.Parse(txtInput.Text);
+            result = double.Parse(txtInput.Text, System.Globalization.CultureInfo.InvariantCulture);
             switch (operation)
             {
                 case Operation.ADD: result = input + result; break;
@@ -81,7 +124,39 @@ namespace Calculator
                 case Operation.NO_OP: break;
                 default: break;
             }
-        } 
+            txtInput.Text = result.ToString();
+            isReal = false;
+        }
+
+        private void ComputeOP_Click(object sender, RoutedEventArgs e)
+        {
+            result = double.Parse(txtInput.Text, System.Globalization.CultureInfo.InvariantCulture);
+            SaveOperationOP((string)((Button)sender).Content);
+                        
+            switch (operation)
+            {
+                case Operation.EXP: result = Math.Exp(result); break;
+                case Operation.SIN: result = Math.Sin(result); break;
+                case Operation.COS: result = Math.Cos(result); break;
+                case Operation.SQRT: result = Math.Sqrt(result); break;
+                case Operation.LOG: result = Math.Log(result); break;
+                case Operation.RECIPROCALLI: result = 1.0 / result; break;
+                case Operation.NO_OP: break;
+                default: break;
+            }
+            txtInput.Text = result.ToString();
+            isReal = false;
+        }
+
+        private void MemOp_Click(object sender, RoutedEventArgs e)
+        {
+            Clean((string)((Button)sender).Content);
+        }
+
+        private void Power_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
         #endregion
     }
 }
